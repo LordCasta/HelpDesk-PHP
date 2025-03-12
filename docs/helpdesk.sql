71aa1ad1323d730159f16bcdce703b82fb988c83
Juan Pablo <114792900+LordCasta@users.noreply.github.com>
2025-03-11 19:26:38 -0500

    se añade el sql de la db sin completar

---
+++ docs/helpdesk.sql
@@ -0,0 +1,256 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 12-03-2025 a las 01:23:38
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `helpdesk`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `td_documento`
+--
+
+CREATE TABLE `td_documento` (
+  `doc_id` int(11) NOT NULL,
+  `tick_id` int(11) NOT NULL,
+  `doc_nom` varchar(255) NOT NULL,
+  `fech_crea` datetime NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `td_documento_detalle`
+--
+
+CREATE TABLE `td_documento_detalle` (
+  `det_id` int(11) NOT NULL,
+  `tickd_id` int(11) NOT NULL,
+  `det_nom` varchar(255) NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_categoria`
+--
+
+CREATE TABLE `tm_categoria` (
+  `cat_id` int(11) NOT NULL,
+  `cat_nom` varchar(150) NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_notificacion`
+--
+
+CREATE TABLE `tm_notificacion` (
+  `not_id` int(11) NOT NULL,
+  `usu_id` int(11) NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_prioridad`
+--
+
+CREATE TABLE `tm_prioridad` (
+  `prio_id` int(11) NOT NULL,
+  `prio_nom` varchar(150) NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_subcategoria`
+--
+
+CREATE TABLE `tm_subcategoria` (
+  `cats_id` int(11) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `cats_nom` varchar(150) NOT NULL,
+  `est` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_ticket`
+--
+
+CREATE TABLE `tm_ticket` (
+  `tick_id` int(11) NOT NULL,
+  `usu_id` int(11) NOT NULL,
+  `cat_id` int(11) NOT NULL,
+  `cats_id` int(11) NOT NULL,
+  `tick_titulo` varchar(255) NOT NULL,
+  `tick_descrip` int(255) NOT NULL,
+  `tick_estado` varchar(50) NOT NULL,
+  `fech_crea` datetime NOT NULL,
+  `usu_asig` int(11) DEFAULT NULL,
+  `fech_asig` datetime DEFAULT NULL,
+  `prio_id` int(11) NOT NULL,
+  `est` int(11) NOT NULL,
+  `tick_coment` varchar(255) DEFAULT NULL,
+  `fech_cierre` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tm_usuario`
+--
+
+CREATE TABLE `tm_usuario` (
+  `usu_id` int(11) NOT NULL,
+  `usu_nom` varchar(150) NOT NULL,
+  `usu_ape` varchar(150) NOT NULL,
+  `usu_pass` varchar(255) NOT NULL,
+  `rol_id` int(11) NOT NULL,
+  `fecha_crea` datetime NOT NULL,
+  `fecha_modi` datetime DEFAULT NULL,
+  `fecha_elim` datetime DEFAULT NULL,
+  `estado` int(11) NOT NULL,
+  `usu_correo` varchar(150) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `tm_usuario`
+--
+
+INSERT INTO `tm_usuario` (`usu_id`, `usu_nom`, `usu_ape`, `usu_pass`, `rol_id`, `fecha_crea`, `fecha_modi`, `fecha_elim`, `estado`, `usu_correo`) VALUES
+(1, 'Juan Pablo', 'Castaño', '123', 0, '2025-03-12 01:21:17', NULL, NULL, 1, 'castanocastano6@gmail.com');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `td_documento`
+--
+ALTER TABLE `td_documento`
+  ADD PRIMARY KEY (`doc_id`);
+
+--
+-- Indices de la tabla `td_documento_detalle`
+--
+ALTER TABLE `td_documento_detalle`
+  ADD PRIMARY KEY (`det_id`);
+
+--
+-- Indices de la tabla `tm_categoria`
+--
+ALTER TABLE `tm_categoria`
+  ADD PRIMARY KEY (`cat_id`);
+
+--
+-- Indices de la tabla `tm_notificacion`
+--
+ALTER TABLE `tm_notificacion`
+  ADD PRIMARY KEY (`not_id`);
+
+--
+-- Indices de la tabla `tm_prioridad`
+--
+ALTER TABLE `tm_prioridad`
+  ADD PRIMARY KEY (`prio_id`);
+
+--
+-- Indices de la tabla `tm_subcategoria`
+--
+ALTER TABLE `tm_subcategoria`
+  ADD PRIMARY KEY (`cats_id`);
+
+--
+-- Indices de la tabla `tm_ticket`
+--
+ALTER TABLE `tm_ticket`
+  ADD PRIMARY KEY (`tick_id`);
+
+--
+-- Indices de la tabla `tm_usuario`
+--
+ALTER TABLE `tm_usuario`
+  ADD PRIMARY KEY (`usu_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `td_documento`
+--
+ALTER TABLE `td_documento`
+  MODIFY `doc_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `td_documento_detalle`
+--
+ALTER TABLE `td_documento_detalle`
+  MODIFY `det_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_categoria`
+--
+ALTER TABLE `tm_categoria`
+  MODIFY `cat_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_notificacion`
+--
+ALTER TABLE `tm_notificacion`
+  MODIFY `not_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_prioridad`
+--
+ALTER TABLE `tm_prioridad`
+  MODIFY `prio_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_subcategoria`
+--
+ALTER TABLE `tm_subcategoria`
+  MODIFY `cats_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_ticket`
+--
+ALTER TABLE `tm_ticket`
+  MODIFY `tick_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tm_usuario`
+--
+ALTER TABLE `tm_usuario`
+  MODIFY `usu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
